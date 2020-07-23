fbc531277d59cd065c4c4e3da822a62ae5d37d8e
kode721 <kerrytheartist31@gmail.com>
2020-07-23 11:16:29 +0200

    deleted my progress

--- myprogress/DBStrings.java
+++
@@ -1,7 +0,0 @@
-package com.example.myprogress;
-
-public class DBStrings {
-
-//    static final String DB_HOST = "10.0.2.2";
-    static final String DB_HOST = "progress-app.com.creative-code.org";
-}
